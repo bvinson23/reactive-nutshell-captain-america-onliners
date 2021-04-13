7545786e38f4bf14f9603ed430b0545731e4632b
bvinson23 <brandonvinson23@gmail.com>
2021-04-13 11:03:51 -0500

    added a little styling just for kicks, also updated article to show all info wanted

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -3,11 +3,13 @@ import "./Article.css";
 
 export const Article = ({ article, handleDeleteArticle }) => {
     return (
-        <article className="article">
+        <article className="card">
             <div className="card-content">
-                <h3>{article.title}</h3>
+                <h3>{article.title}-{article.user.name}</h3>
                 <p>{article.synopsis}</p>
+                <p>{article.timestamp}</p>
                 <button type="button" onClick={() => handleDeleteArticle(article.id)}>Delete</button>
+                <p><a href={article.url}>{article.url}</a></p>
             </div>
         </article>
     )
