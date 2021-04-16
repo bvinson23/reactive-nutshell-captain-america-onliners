4a831ba74119e13f1252442514dd5a84953b0857
bvinson23 <brandonvinson23@gmail.com>
2021-04-16 13:53:43 -0500

    cleaned up some things

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -5,18 +5,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const MessageCard = ({ message, handleDeleteMessage }) => {
+    const currentUser = parseInt(sessionStorage.getItem("nutshell_user"));
+
     return (
         <>
             <div className="chat-message">
                 <p>{message.user.name} said, "{message.chat}"</p>
-                <div className="button-box">
-                    <Link to={`/messages/${message.id}/edit`}>
-                        <button type="button">
-                            Edit
-                        </button>
-                    </Link>
-                    <button type="button" onClick={() => handleDeleteMessage(message.id)}>Delete</button>
-                </div>
+
+                {message.userId === currentUser ?
+                    <>
+                        <div className="button-box">
+                            <Link to={`/messages/${message.id}/edit`}>
+                                <button type="button">
+                                    Edit
+                                </button>
+                            </Link>
+                            <button type="button" onClick={() => handleDeleteMessage(message.id)}>Delete</button>
+                        </div>
+                    </>
+                    : null}
             </div>
         </>
     )
