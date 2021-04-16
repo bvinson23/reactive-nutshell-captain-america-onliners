91109e7d70e936510e6c457c614ad72c5fb10546
Carter Culkin <carter.culkin@gmail.com>
2021-04-16 14:29:41 -0500

    centered and now h6 instead of h9

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -14,17 +14,17 @@ export const Article = ({ article, handleDeleteArticle, currentUser }) => {
                     <article className="articleCard">
                         <div className="card-content">
                             <h3>{article.title}</h3>
-                            <h9>You posted this</h9>
+                            <h6>You posted this</h6>
                             <p></p>
                             <p>{article.synopsis}</p>
                             <p>{article.timestamp}</p>
+                            <p><a href={article.url}>{article.url}</a></p>
                             <Link to={`/${article.id}/edit`}>
                                 <button type="button">
                                     Edit
                                 </button>
                             </Link>
                             <button type="button" onClick={() => handleDeleteArticle(article.id)}>Delete</button>
-                            <p><a href={article.url}>{article.url}</a></p>
                         </div>
                     </article>
                 </> :
@@ -32,7 +32,7 @@ export const Article = ({ article, handleDeleteArticle, currentUser }) => {
                     <article className="articleCard__friend">
                         <div className="card-content">
                             <h3>{article.title}</h3>
-                            <h9>Posted by your friend {article.user.name}</h9>
+                            <h6>Posted by your friend {article.user.name}</h6>
                             <p></p>
                             <p>{article.synopsis}</p>
                             <p>{article.timestamp}</p>
