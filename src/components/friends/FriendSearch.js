0a52a9ca18faf52d2a2a9cdf71facaa66949ac2d
Cody_Jones <cbjones93@gmail.com>
2021-04-16 10:24:20 -0500

    add friend now reroutes to friends list

--- src/components/friends/FriendSearch.js
+++ src/components/friends/FriendSearch.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {FriendList} from './FriendList'
+import {Link} from 'react-router-dom'
 
 export const SearchBar =({ handleAddFriend, search }) => {
     return (
       <div className="SearchBox">
         <h3>{search.name}</h3>
-        <button type="button" className="searchbutton" onClick={() => handleAddFriend(search.id)}>Add Friend</button>
+        <Link to= {`/friends`}> <button type="button" className="searchbutton" onClick={() => handleAddFriend(search.id)}>
+        Add Friend</button> </Link>
       </div>
 
     );
