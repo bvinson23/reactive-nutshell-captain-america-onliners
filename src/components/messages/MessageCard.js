b6106a0189a1a0cb0cdc2891e954f7ccb4b7a80b
prestonshotts <shotts.preston@gmail.com>
2021-04-19 09:20:31 -0500

    getting help from team for css bug

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -3,6 +3,7 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import "./MessageCard.css"
 
 
 export const MessageCard = ({ message, handleDeleteMessage, handleAddFriend }) => {
@@ -10,7 +11,8 @@ export const MessageCard = ({ message, handleDeleteMessage, handleAddFriend }) =
     return (
         <>
             <div className="chat-message">
-                <p>{message.user.name} said, "{message.chat}"</p>
+                <p>{message.user.name}</p> 
+                <p className="chatMessage">{message.chat}</p>
                 <div className="button-box">
                     <button type="button" onClick={() => {
                     if (window.confirm(`Do you want to add ${message.user.name} as a friend ?`)) {
