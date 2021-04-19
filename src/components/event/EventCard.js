f820161ac5818558bfc7238c93079e834bcba7f2
Cody_Jones <cbjones93@gmail.com>
2021-04-19 13:20:47 -0500

    was making changes to weather but need new branch

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -1,7 +1,7 @@
 // Author: Cody Jones, Carter Culkin
 import React from "react";
 import {useHistory} from 'react-router-dom'
-export const EventCard = ({ event, deleteEvent }) => {
+export const EventCard = ({ getDailyWeather, event, deleteEvent }) => {
 const history = useHistory();
 
     return (
@@ -13,7 +13,7 @@ const history = useHistory();
                     <p className="eventLocation"> Location: {event.location}</p>
                     <p className="eventZipcode">Zipcode: {event.zipcode}</p>
                     <p className="eventUserId">User: {event.userId}</p>
-                
+                    <button type="button" onClick={() => getDailyWeather(event)}>Show Weather</button>
                     <button type="button" onClick={() => history.push(`events/${event.id}/edit`)}>Edit</button>
                     <button type="button" onClick={() => deleteEvent(event.id)}>Delete</button>
                 </h3>
