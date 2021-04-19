a1629a1a2c6f61d3eb24ec3c2ca203a33b1e4da9
Carter Culkin <carter.culkin@gmail.com>
2021-04-19 16:04:00 -0500

    Should be good now

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -1,6 +1,7 @@
 // Author: Cody Jones, Carter Culkin
 import React from "react";
 import {useHistory} from 'react-router-dom'
+import "./Event.css"
 export const EventCard = ({ getDailyWeather, event, deleteEvent }) => {
 const history = useHistory();
     return (
@@ -12,9 +13,9 @@ const history = useHistory();
                     <p className="eventLocation"> Location: {event.location}</p>
                     <p className="eventZipcode">Zipcode: {event.zipcode}</p>
                     <p className="eventUserId">User: {event.userId}</p>
-                    <button type="button" onClick={() => getDailyWeather(event)}>Show Weather</button>
-                    <button className="eventButtons"type ="button" onClick={() => props.history.push(`events/${props.event.id}/edit`)}>Edit</button>
-                    <button className="eventButtons" type ="button" onClick={() => props.deleteEvent(props.event.id)}>Delete</button>
+                    <button className="eventButton" type="button" onClick={() => getDailyWeather(event)}>Show Weather</button>
+                    <button className="eventButton" type="button" onClick={() => history.push(`events/${event.id}/edit`)}>Edit</button>
+                    <button className="eventButton" type="button" onClick={() => deleteEvent(event.id)}>Delete</button>
                 </h3>
             </div>
         </div>
